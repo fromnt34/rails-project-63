5bb2d95eb14ae9019e85312830647ba2fc362a89
Dmitry Kurtsev <dmitrykurtsev34@gmail.com>
2022-12-08 16:07:38 +0300

    Refactoring

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -4,7 +4,7 @@ module HexletCode
   autoload :VERSION, 'hexlet_code/version'
   autoload :Html, 'hexlet_code/html'
 
-  def self.form_for(user, **attributes, &)
-    Html::SpecialTags::Form.new(user, **attributes, &).to_s
+  def self.form_for(object, **attributes, &)
+    Html::SpecialTags::Form.new(object, **attributes, &).to_s
   end
 end
